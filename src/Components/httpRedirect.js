1d460a8758bc92438fcba124ba727a877b654632
Travis Smith <Travis.Smith2@gmail.com>
2018-07-14 17:36:42 -0400

    httpredirect heroku

--- src/Components/httpRedirect.js
+++ src/Components/httpRedirect.js
@@ -2,8 +2,8 @@ import * as React from 'react'
 
 class HttpRedirect extends React.Component {
   render() {
-    if (window.location.protocol === 'https') {
-      const newLocation = window.location.href.replace('https', 'http')
+    if (window.location.protocol === 'https:') {
+      const newLocation = window.location.href.replace('https:', 'http:')
       window.location.href = newLocation
     }
     return <React.Fragment />
