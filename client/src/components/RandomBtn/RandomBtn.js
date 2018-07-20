213861ce3543ccc04fe506aedbb7acada9ee3154
Stacy M <geekgirlbeta@gmail.com>
2018-07-20 15:52:05 -0400

    more components

---
+++ client/src/components/RandomBtn/RandomBtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./RandomBtn.css";
+
+const RandomBtn = props => (
+  <button className="random-btn" {...props}>
+    Create Random Character
+  </button>
+);
+
+export default RandomBtn;
