f862ba1f4b04eba3825487a9d05297edf1269b04
Travis Smith <Travis.Smith2@gmail.com>
2018-07-21 13:31:24 -0400

    MVP!

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+//const express = require("express");
+//const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 //const routes = require("./client/src/routes")
-const app = express();
-const PORT = process.env.PORT || 3001;
+//const app = express();
+//const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client"));
-}
+//if (process.env.NODE_ENV === "production") {
+//  app.use(express.static("client"));
+//}
 // Add routes, both API and view
 //app.use(routes);
 
@@ -19,6 +19,6 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+//app.listen(PORT, function() {
+//  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+//});
